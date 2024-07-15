1d5a7af8b5e03bb22164c49fabfaae540743d1c4
3abdel7amed <ahmed.abdelhamid@badgewell.com>
2024-07-15 10:22:54 +0300

    initialize the app

---
+++ src/app/landing/landing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RouteTaskRoutingModule } from './landing-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RouteTaskRoutingModule],
+})
+export class LandingModule {}
