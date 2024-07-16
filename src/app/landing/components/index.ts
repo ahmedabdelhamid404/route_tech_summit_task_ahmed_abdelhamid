542edd987728e6f40da18d961be9cd556b887250
ahmedabdelhamid404 <ahmedabdelhamid404@users.noreply.github.com>
2024-07-16 12:53:37 +0000

    deploy: 5c96348524ccf37214ff382da577287c192eae22

--- src/app/landing/components/index.ts
+++
@@ -1,7 +0,0 @@
-import { CustomersTableComponent } from './customers-table/customers-table.component';
-import { ChartDialogComponent } from './chart-dialog/chart-dialog.component';
-
-export const COMPONENTS = [CustomersTableComponent, ChartDialogComponent];
-
-export { CustomersTableComponent } from './customers-table/customers-table.component';
-export { ChartDialogComponent } from './chart-dialog/chart-dialog.component';
--- src/app/landing/services/index.ts
+++
@@ -1,5 +0,0 @@
-import { CustomersService } from './customers.service';
-
-export const SERVICES = [CustomersService];
-
-export { CustomersService } from './customers.service';
