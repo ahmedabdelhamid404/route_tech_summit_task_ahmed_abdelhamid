18f372992ef3bd303144dcbb7d765ab0f9785c16
ahmedabdelhamid404 <ahmedsoftwaredev3@gmail.com>
2024-08-16 11:15:16 +0000

    Deploy WAR file to production branch

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'landing',
+    loadChildren: () =>
+      import('./landing/landing.module').then((m) => m.LandingModule),
+  },
+  {
+    path: '',
+    redirectTo: 'landing',
+    pathMatch: 'full',
+  },
+];
---
+++ src/app/landing/pipes/index.ts
@@ -0,0 +1,5 @@
+import { GetNamePipe } from './get-name.pipe';
+
+export const PIPES = [GetNamePipe];
+
+export { GetNamePipe } from './get-name.pipe';
